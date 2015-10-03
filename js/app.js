f14bdd08fe468b284bbae70ed0dd3cd2d8bbc7aa
DavidCiccone <davidkciccone@gmail.com>
2015-10-03 15:01:41 -0400

    Functioning

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ var Enemy = function Enemy(x, y) {
     // a helper we've provided to easily load images
     this.x = x;
     this.y = y;
+    this.width = 50;
+    this.height = 171;
     this.sprite = 'images/enemy-bug.png';
+    
   
 }
 
@@ -17,11 +20,15 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
 
-//Bug speed
-this.x = this.x + (Math.random() * (500 - 1)) * dt;
-    //Resets bugs back to the start at or behind -120 px
+//Move the Bugs
+var max = 500;
+var min = 1;
+
+this.x = this.x + Math.floor(Math.random() * (max - min) + min) * dt;
+   
+//Resets bugs back to the start at or behind -120 px
     if (this.x > 800) {
-       this.x = ((Math.random() * 500) && -120);
+       this.x = ((Math.random()) && -120);
         }
     }
 
@@ -31,62 +38,60 @@ Enemy.prototype.render = function() {
 };
 //  ---Player Class---
 var Player = function Player(x, y) {
-this.x = 200;
-this.y = 400;
+this.x = x;
+this.y = y;
+this.width = 50;
+this.height = 171;
 this.sprite = 'images/char-boy.png';
-
 }
 
-Player.prototype.update = function()  {}
+Player.prototype.update = function()  {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-//var enemyGroup = allEnemies;
-  //  var playerCol = Player;
-    //var dims = {x: 50, y: 50, w: 50, h: 50};
-    //if (enemyGroup.x < playerCol.x + playerCol.w &&
-      //  enemyGroup.x + enemyGroup.w > playerCol.x &&
-       // enemyGroup.y < playerCol.y + playerCol.h &&
-      //  enemyGroup.h + enemyGroup.y > playerCol.y) {
-      //  this.x = 200;
-       // this.y = 400;
-    //}
+
+//keeps player within canvas boundaries
+if (this.x >= 455) {this.x = 400}
+if (this.x <= 0) {this.x = 0}
+if (this.y > 400) {this.y = 400}
+
+//Resets the player once it hits the water
+if(this.y < 0) { this.x = 200, this.y = 400}
+
+//Enemy collision detection
+for(var i = 0; i < allEnemies.length; i++){
+if (this.x < allEnemies[i].x && this.x > allEnemies[i].x - this.width 
+    && this.y < allEnemies[i].y && this.y > allEnemies[i].y - this.height) 
+{this.x = 200, this.y = 400}
+}
+
+
+}
+
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-//player movement
-Player.prototype.handleInput = function(directionKey) {
 
-if(directionKey === 'left'){
-this.x -= 100;
-    }
-    if(directionKey === 'right'){
-this.x += 100;
-    } 
-if(directionKey === 'up'){
-this.y -= 90;
-    }
-    if(directionKey === 'down'){
-this.y += 90;
-    }
+Player.prototype.handleInput = function(Key) {
+//player movement keys
+if(Key === 'left'){this.x -= 100;}
+if(Key === 'right'){this.x += 100;} 
+if(Key === 'up'){this.y -= 90;}
+if(Key === 'down'){this.y += 90;}
 }
 
 
-
-//keeps player within canvas boundaries
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy (-400,65), new Enemy (-275,145), new Enemy (-120,225), new Enemy (-650,145), new Enemy (-1000,65)];
+var allEnemies = [new Enemy (-400,65), new Enemy (-275,145), new Enemy (-120,225), new Enemy (-650,145), new Enemy (-1000,65), new Enemy (-850,65)];
+var player = new Player(200, 400);
 
-
-var player = new Player();
-
-
+//, new Enemy (-275,145), new Enemy (-120,225), new Enemy (-650,145), new Enemy (-1000,65), new Enemy (-850,65)
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -27,7 +27,7 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    document.getElementById('someBox').appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
@@ -80,7 +80,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        //checkCollisions();
     }
 
     /* This is called by the update function  and loops through all of the
